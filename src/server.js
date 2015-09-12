a7f827baaf60ad6f5c0bbed94e7140e574ac7954
James Conkling <james.lane.conkling@gmail.com>
2015-09-12 12:35:16 -0700

    serve boilerplate client to all root requests

--- src/server.js
+++ src/server.js
@@ -1,22 +1,26 @@
+require('dotenv').load();
 var express = require('express');
 var app = express();
 var morgan = require('morgan');
 var cors = require('cors');
 var bodyParser = require('body-parser');
-require('dotenv').load();
 
+// initialize middleware
+app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+// initialize and set up routers
 var authRouter = express.Router();
 var userRouter = express.Router();
 require('./auth/authRouter')(authRouter);
 require('./user/userRouter')(userRouter);
 
-app.use(cors());
-app.use(morgan('dev'));
-app.use(bodyParser.json());
-
+app.use('/', express.static(__dirname + '/client'));
 app.use('/api/auth', authRouter);
 app.use('/api/users', userRouter);
 
+// start server
 var server = app.listen(process.env.PORT, function(){
   var host = server.address().address;
   var port = server.address().port;
